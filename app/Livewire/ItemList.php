ce37abcec23eda3bd5bfa768161ab5fa0595d190
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-11-22 23:11:54 +0200

    wip

--- app/Livewire/ItemList.php
+++ app/Livewire/ItemList.php
@@ -8,14 +8,29 @@ class ItemList extends Component
 {
 
     public $items = [];
+    public $name = '';
+    public $description = '';
 
     public function mount()
     {
         $this->items = \App\Models\Item::all();
     }
-
+    
     public function render()
     {
         return view('livewire.item-list');
+    }
+
+    public function create()
+    {
+        \App\Models\Item::create([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+
+        $this->name = '';
+        $this->description = '';
+
+        $this->items = \App\Models\Item::all();
     }
 }
--- resources/views/livewire/item-list.blade.php
+++ resources/views/livewire/item-list.blade.php
@@ -1,5 +1,12 @@
 <div>
     @foreach($items as $item)
-        <div>{{ $item->name }} {{ $item->status->name }} {{ $item->status->color }}</div>
+        <div>{{ $item->name }} {{ $item->status?->name }} {{ $item->status?->color }}</div>
     @endforeach
+    <div>
+        <form wire:submit.prevent="create">
+            <input type="text" wire:model="name">
+            <input type="text" wire:model="description">
+            <button type="submit">{{__('store')}}</button>
+        </form>
+    </div>
 </div>
